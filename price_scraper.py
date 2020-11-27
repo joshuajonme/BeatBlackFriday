ce4106f6b3f9645791abd59f2c56484de808d3fa
Joshua Coronel <Joshuajonme@gmail.com>
2020-11-27 00:07:47 -0500

    fixed web scraping

--- price_scraper.py
+++ price_scraper.py
@@ -1,6 +1,5 @@
 import requests 
 import json
-# from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from product import Product
@@ -12,14 +11,15 @@ from web_driver_conf import set_browser_as_incognito
 from web_driver_conf import set_automation_as_head_less
 
 URL = "http://www.amazon.com/"
-NUMBER_OF_PAGES_TO_SEARCH = 5
+NUMBER_OF_PAGES_TO_SEARCH = 1
 QUESTION_PRODUCT = "What are you looking for?\n:"
+PRODUCT_PATH = '//*[@id="search"]/div[1]/div[2]/div/span[3]/div[2]/div'
 search_term = str(input(QUESTION_PRODUCT))
 
 biggest_discount = 0.0
 lowest_price = 0.0
-chepest_product = Product("", "", "", "")
-best_deal_product = Product("", "", "", "")
+chepest_product = Product("", "", "", "", "", "")
+best_deal_product = Product("", "", "", "", "", "")
 search_terms = search_term.split(" ")
 
 options = get_web_driver_options()
@@ -43,34 +43,43 @@ while True:
             driver.get(driver.current_url + "&page=" + str(page))
         except:
             break
-
-    for i in driver.find_elements_by_xpath('//*[@id="search"]/div[1]/div[2]/div/span[3]/div[2]'):
-        counter = 0
-        for element in i.find_elements_by_xpath('//div/span/div/div/div/div'):
-            should_add = True
-            name = ""
-            price = ""
-            prev_price = ""
-            link = ""
+    
+    for i in driver.find_elements_by_xpath(PRODUCT_PATH):
+        should_add = True
+        name = ""
+        price = ""
+        prev_price = ""
+        link = ""
+        discount = 0.0
+        # rating = ""
+        prime = False
+        try:
+            h2tag = i.find_element_by_tag_name('h2')
+            name = h2tag.text
+            price = convert_price_toNumber(i.find_element_by_class_name('a-price').text)
+            link = h2tag.find_element_by_tag_name('a').get_attribute("href")
+            # rating = 
             try:
-                name = i.find_elements_by_tag_name('h2')[counter].text
-                price = convert_price_toNumber(element.find_element_by_class_name('a-price').text)
-                if price and counter < 20:
-                    print(element.find_element_by_class_name('a-price').text)
-                link = i.find_elements_by_xpath('//h2/a')[counter].get_attribute("href")
-                try:
-                    prev_price = convert_price_toNumber(element.find_element_by_class_name('a-text-price').text)
-                except:
-                    Exception()
-                    prev_price = price
+                prime_element = i.find_element_by_class_name("a-icon-prime")
+                print(prime_element)
+                prime = True
             except:
-                # print("exception")
-                should_add = False
-            product = Product(name, price, prev_price, link)
-            if should_add:
-                products.append(product)
-                # print(products)
-            counter = counter + 1
+                Exception()
+            try:
+                prev_price = convert_price_toNumber(i.find_element_by_class_name('a-text-price').text)
+                discount = (prev_price-price)/prev_price*100
+            except:
+                Exception()
+                prev_price = price
+        except:
+            # print("exception")
+            should_add = False
+        
+        product = Product(name, price, prev_price, discount, link, prime)
+        if should_add:
+            products.append(product)
+            # print(products)
+            
     
     page = page - 1
     if page == 0:
@@ -78,12 +87,11 @@ while True:
     print(page)
 
 run = 0
-
 for product in products:
     not_right = False
-    for word in search_terms:
-        if word.lower() not in product.name.lower():
-            not_right = True
+    # for word in search_terms:
+    #     if word.lower() not in product.name.lower():
+    #         not_right = True
     if not not_right:
         if run == 0:
             lowest_price = product.price
@@ -92,9 +100,9 @@ for product in products:
         elif product.price < lowest_price:
             lowest_price = product.price
             chepest_product = product
-        discount = product.prev_price - product.price
-        if discount > biggest_discount:
-            biggest_discount = discount
+        if product.discount > biggest_discount:
+            biggest_discount = product.discount
+            print(product.discount)
             best_deal_product = product
 
 with open('products.json', 'w') as json_file:
